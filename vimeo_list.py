c7881ea4107f2b01981ac65f97862380d0041a9e
Fernando Ortega Gorrita <fernandoog@gmail.com>
2023-02-28 23:53:43 +0100

    add example

--- vimeo_list.py
+++ vimeo_list.py
@@ -1,7 +1,9 @@
 from vimeo_downloader import Vimeo
 
 # Replace these with other list of videos you want to download
-videos = ['https://vimeo.com/766411266']
+videos = ['https://player.vimeo.com/video/795204937',
+          'https://vimeo.com/504420495',
+          'https://vimeo.com/481277944']
 
 for video in videos:
     v = Vimeo(video)
@@ -9,6 +11,8 @@ for video in videos:
 
     # Selecting and downloading '720p' video
     for s in stream:
+        if s.quality == 'Auto':
             s.download(download_directory='video', filename=v.metadata.title)
+            break
     else:  # If the loop never break
         print('quality not found')
